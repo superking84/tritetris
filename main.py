01e047cba9c9cec0971e255e147c81f44b4bb1d9
Frank Harvey <frankharvey88@gmail.com>
2014-01-21 13:59:04 -0600

    added methods and standardized method names.
    Added the following methods to objects.py:
    Field._create_random_block
    Field.get_block_from_queue
    Field.add_block_to_queue
    
    Renamed Block.place_on_field to Block._set_locations.

---
+++ main.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import pygame
+from pygame.locals import *
+import objects
+from colors import *
+
+x = 450
+y = 100
+os.environ['SDL_VIDEO_WINDOW_POS'] = '%d, %d' % (x, y)
+
+pygame.init()
+clock = pygame.time.Clock()
+
+# draw constants
+SCREEN_WIDTH, SCREEN_HEIGHT = 480, 600
+FIELD_WIDTH = SCREEN_WIDTH * 5 / 8
+NUM_COLUMNS = 20
+NUM_ROWS = 50
+CELL_HEIGHT = CELL_WIDTH = FIELD_WIDTH / NUM_COLUMNS
+
+DISPLAYSURFACE = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("TriteTris!")
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+    
+def draw_field(surface, field):
+    '''
+    Draws the given field and its current state to the display surface.
+    '''
+    
+    # DEBUG CODE: Show grid as a visual aid during coding
+    for i in range(1,field.num_rows+1):
+        pygame.draw.line(surface, BLACK, (0, CELL_HEIGHT * i), (FIELD_WIDTH, CELL_HEIGHT * i))
+    for j in range(1,field.num_columns+1):
+        pygame.draw.line(surface, BLACK, (CELL_WIDTH * j, 0), (CELL_WIDTH  * j, SCREEN_HEIGHT))
+    # END DEBUG CODE
+    
+    
+def play():
+    field = objects.Field(NUM_ROWS, NUM_COLUMNS)
+    block = objects.Block(field, 'I', RED)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            if event.type == KEYUP:
+                if event.key == K_ESCAPE:
+                    terminate()
+                    
+        DISPLAYSURFACE.fill(WHITE)
+        draw_field(DISPLAYSURFACE, field)
+        
+        pygame.display.update()
+        
+if __name__ == '__main__':
+    play()
--- objects.py
+++ objects.py
@@ -1,3 +1,4 @@
+import random
 import colors
 
 class Field(object):
@@ -12,11 +13,34 @@ class Field(object):
         self.num_rows = num_rows
         self.num_columns = num_columns
         self.cells = []
+        self.active_block = None
+        self.block_queue = []
 
         for i in range(num_rows):
             self.cells.append([])
             for j in range(num_columns):
                 self.cells[i].append(None)
+                
+    def __repr__(self):
+        output = '\n'.join([str([cell for cell in row]) for row in self.cells])
+        
+        return "Field state:\n" + output
+        
+    def _create_random_block(self):
+        return Block(self, random.choice(Block._types), colors.get_random_color())
+        
+    def get_block_from_queue(self):
+        if self.block_queue:
+            return self.block_queue.pop(0)
+        else:
+            print "Queue is empty!"
+        
+    def add_block_to_queue(self):
+        self.block_queue.append(self._create_random_block())
+    
+    __str__ = __repr__
+    
+    
 
 
 class Block(object):
@@ -28,47 +52,50 @@ class Block(object):
     '''
 
     _type_offsets = {
-                     ‘I’: {0:  [(0,1),(0,2),(0,3)],      90: [(1,0),(2,0),(3,0)]},
-                     ‘O’: {0:  [(0,1),(1,0),(1,1)]},
-                     ‘T’: {0:  [(1,-1),(1,0),(1,1)],     90: [(-1,-1),(0,-1),(1,-1)],
+                     'I': {0:  [(0,1),(0,2),(0,3)],      90: [(1,0),(2,0),(3,0)]},
+                     'O': {0:  [(0,1),(1,0),(1,1)]},
+                     'T': {0:  [(1,-1),(1,0),(1,1)],     90: [(-1,-1),(0,-1),(1,-1)],
                           180: [(-1,-1),(-1,0),(-1,1)], 270: [(-1,1),(0,1),(1,1)]},
-                     ‘S’: {0:  [(0,1),(1,-1),(1,0)],     90: [(-1,0),(0,1),(1,1)]},
-                     ‘Z’: {0:  [(0,1),(1,1),(1,2)],      90: [(1,-1),(1,0),(2,-1)]},
-                     ‘J’: {0:  [(1,0),(2,-1),(2,0)],     90: [(-1,-2),(0,-2),(0,-1)],
+                     'S': {0:  [(0,1),(1,-1),(1,0)],     90: [(-1,0),(0,1),(1,1)]},
+                     'Z': {0:  [(0,1),(1,1),(1,2)],      90: [(1,-1),(1,0),(2,-1)]},
+                     'J': {0:  [(1,0),(2,-1),(2,0)],     90: [(-1,-2),(0,-2),(0,-1)],
                           180: [(-2,0),(-2,1),(-1,0)],  270: [(0,1),(0,2),(1,2)]},
-                     ‘L’: {0:  [(1,0),(2,0),(2,1)],      90: [(0,-2),(0,-1),(1,-2)],
+                     'L': {0:  [(1,0),(2,0),(2,1)],      90: [(0,-2),(0,-1),(1,-2)],
                           180: [(-2,-1),(-2,0),(-1,0)], 270: [(-1,2),(0,1),(0,2)]}
                      }
     _types = _type_offsets.keys()
 
     def __init__(self, field, type, color):
-        self.locations = [] # this will hold four tuples indicating the block’s location once init’d
+        self.locations = [] # this will hold four tuples indicating the block's location once init'd
         self.field = field
         self.color = color # this should be in RGB format (or a pygcolors constant)
         self.type = type
         self.orientation = 0
+        
+    def __repr__(self):
+        return 'Block type %s' % self.type
+        
+    __str__ = __repr__
 
-    def place_on_field(self, init_location, orientation=0):
+    def _set_locations(self, init_location):
         '''
-        This function applies a Block’s starting location so that it can appear on the
+        This function applies a Block's starting location so that it can appear on the
         game field.
         Each Block type has three offsets.  init_location and these three offsets
-        represent the location of each of the Block’s four pieces.
+        represent the location of each of the Block's four pieces.
         '''
         self.locations = []
         self.locations.append(init_location)
-        for offset in _type_offsets[self.type][orientation]:
+        for offset in _type_offsets[self.type][self.orientation]:
             new_location = (init_location[0]  + offset[0], init_location[1] + offset[1])
             self.locations.append(new_location)
             
-    def rotate(self, forwards=True):
+    def rotate(self, clockwise=True):
         '''
-        Tilts the Block clockwise 90 degrees if forwards is set to True(set to True
+        Tilts the Block clockwise 90 degrees if clockwise is set to True(set to True
         by default), counterclockwise if False.
         '''
-        if forwards:
+        if clockwise:
             self.orientation = (self.orientation + 90) % 360
         else:
             self.orientation = (self.orientation - 90) % 360
-            
-# TODO: Begin to build graphical interface for game
