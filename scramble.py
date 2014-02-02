06d19da18ab1fc22e25eaef8fcbbbc43d97f93fa
Frank Harvey <frankharvey88@gmail.com>
2014-02-02 13:18:31 -0600

    Updated gameplay.
    I added the following:
    1. The game now shows you the next block in the queue;
       a bit off-center but that can be fixed.
    2. scramble.py, a small module with a single class that takes
       a single string and can return a scrambled version of it.
       Its only real use for right now is to jazz up the pause message
       a little bit (I'm a dork).

---
+++ scramble.py
@@ -0,0 +1,9 @@
+import random
+from itertools import permutations as perm
+
+class Scrambler(object):
+    def __init__(self, word):
+        self.perms = [item for item in perm(word, len(word))]
+        
+    def get_scrambled_word(self):
+        return ''.join(random.choice(self.perms))
